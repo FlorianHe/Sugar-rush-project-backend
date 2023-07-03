068eca459fc14a3506e2fd5b2494c353a940594c
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-07-03 16:22:51 +0200

    create commentService with repository

---
+++ src/main/java/com/templateproject/api/service/CommentService.java
@@ -0,0 +1,48 @@
+package com.templateproject.api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.templateproject.api.entity.Comment;
+import com.templateproject.api.repository.CommentRepository;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+    // TODO Add a method to find all comments by the user id
+    // TODO List<Comment> getAllCommentsByUserId(Long userId);
+
+    public Comment createComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public Comment updateComment(Long id, Comment updatedComment) {
+        Comment existingComment = commentRepository.findById(id).orElse(null);
+        if (existingComment != null) {
+            existingComment.setText(updatedComment.getText());
+            existingComment.setAuthor(updatedComment.getAuthor());
+            return commentRepository.save(existingComment);
+        }
+        return null;
+    }
+
+    public boolean deleteComment(Long id) {
+        Comment comment = commentRepository.findById(id).orElse(null);
+        if (comment != null) {
+            commentRepository.delete(comment);
+            return true;
+        }
+        return false;
+    }
+}
