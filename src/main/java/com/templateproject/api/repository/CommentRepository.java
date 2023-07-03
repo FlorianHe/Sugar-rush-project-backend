82ec787868294b67a5fe58cb23cfc7152d23c704
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-07-03 16:22:15 +0200

    update repository and controller with service

--- src/main/java/com/templateproject/api/repository/CommentRepository.java
+++ src/main/java/com/templateproject/api/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.templateproject.api.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,8 @@ import com.templateproject.api.entity.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    // TODO Add a method to find all comments by the user id
+    // TODO List<Comment> findAllByUserId(Long userId);
 
 }
