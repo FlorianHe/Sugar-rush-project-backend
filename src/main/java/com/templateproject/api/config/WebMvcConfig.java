e202f279f4915eb4b2de8c889f4d6ca2513dff91
FlorianHe <florian.helaine@epitech.eu>
2023-09-05 13:35:10 +0200

    Adding new selin url to the cors conf

--- src/main/java/com/templateproject/api/config/WebMvcConfig.java
+++ src/main/java/com/templateproject/api/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://sugarrush.helaine.eu", "http://selin.ovh")
+        .allowedOrigins("http://sugarrush.helaine.eu", "http://selin.ovh", "http://sugarrush.selin.ovh")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
         .allowedHeaders("Content-Type", "Date", "Total-Count", "loginInfo", "Authorization")
         .allowCredentials(true)
